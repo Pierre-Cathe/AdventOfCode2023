4190650f650e4368fd6ca2ee8eb952ded405f82b
Pierre Cathe <pierre.cathe@gmail.com>
2023-12-02 11:01:53 +0100

    Day 2 part 1 cleaner

--- day2/day2.py
+++ day2/day2.py
@@ -1,34 +1,44 @@
+from functools import partial
+
 INPUT_FILE = './input'
 PART_1_LIMITS = {'red': 12, 'green': 13, 'blue': 14}
 
 
-def is_showing_valid(showing, limits):
+def parse_showing(showing):
+    color_values = {}
     for color_group in showing.split(', '):
         color_value, color = color_group.split(' ')
-        if limits[color] < int(color_value):
-            return False
-    return True
+        color_values[color] = int(color_value)
+    return color_values
 
 
-def check_valid_game(line, limits):
+def parse_game(line):
     game_title, game_showings = line.split(': ')
     game_id = int(game_title.split(' ')[1])
+    showings = []
     for showing in game_showings.split('; '):
-        if not is_showing_valid(showing, limits):
-            return None
+        showings.append(parse_showing(showing))
+    return game_id, showings
+
+
+def get_games_from_file(filename):
+    games = []
+    with open(filename) as input:
+        for line in input:
+            games.append(parse_game(line.rstrip()))
+    return games
+
+
+def get_id_if_valid(game, limits):
+    game_id, showings = game
+    for showing in showings:
+        for color, color_value in showing.items():
+            if limits[color] < color_value:
+                return 0
     return game_id
 
 
-def get_valid_games_from_file(filename, limits):
-    valid_games = []
-    with open(filename) as input:
-        for line in input:
-            game_id = check_valid_game(line.rstrip(), limits)
-            if game_id is not None:
-                valid_games.append(game_id)
-    return valid_games
-
-
 if __name__ == "__main__":
-    valid_games = get_valid_games_from_file(INPUT_FILE, PART_1_LIMITS)
-    print(sum(valid_games))
+    all_games = get_games_from_file(INPUT_FILE)
+    all_valid_games = list(map(partial(get_id_if_valid, limits=PART_1_LIMITS), all_games))
+    print(sum(all_valid_games))
