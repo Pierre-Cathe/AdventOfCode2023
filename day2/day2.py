688858d5a415d55f877064b9d157e6fde96333c1
Pierre Cathe <pierre.cathe@gmail.com>
2023-12-02 11:13:38 +0100

    Day 2 part 2

--- day2/day2.py
+++ day2/day2.py
@@ -38,7 +38,24 @@ def get_id_if_valid(game, limits):
     return game_id
 
 
+def get_minimal_set_power(game):
+    minimal_set = {}
+    _, showings = game
+    for showing in showings:
+        for color, color_value in showing.items():
+            if not color in minimal_set:
+                minimal_set[color] = color_value
+            if color_value > minimal_set[color]:
+                minimal_set[color] = color_value
+    set_power = 1
+    for _, color_value in minimal_set.items():
+        set_power *= color_value
+    return set_power
+
+
 if __name__ == "__main__":
     all_games = parse_games_from_file(INPUT_FILE)
     all_valid_games = list(map(partial(get_id_if_valid, limits=PART_1_LIMITS), all_games))
     print(sum(all_valid_games))
+    game_minimal_sets_powers = list(map(get_minimal_set_power, all_games))
+    print(sum(game_minimal_sets_powers))
