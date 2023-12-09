963d82e8826a8945455c8c2d1add572f5e0ae106
Pierre Cathe <pierre.cathe@gmail.com>
2023-12-09 10:05:59 +0100

    Day 8 part 2

--- day8/day8.py
+++ day8/day8.py
@@ -1,7 +1,7 @@
-
+import math
 
 FILENAME = './input'
-# FILENAME = './example2'
+# FILENAME = './example3'
 
 
 def add_to_network(network, line):
@@ -38,9 +38,50 @@ def steps_until_end(network, instructions):
     return steps
 
 
+def steps_until_end_ghost(network, instructions):
+    steps = 0
+    current_nodes = [x for x in network.keys() if x[-1] == 'A']
+    print(current_nodes)
+    print(instructions)
+    has_found_end = False
+    counters = [[] for node in current_nodes]
+    current_counters = [0 for node in current_nodes]
+    while steps <= len(instructions) * len(network):
+        # if steps % (283*1000) == 0:
+        #     print(steps//283)
+        for instruction in instructions:
+            steps += 1
+            direction = 'left' if instruction == 'L' else 'right'
+            has_found_end = True
+            for i in range(len(current_nodes)):
+                current_nodes[i] = network[current_nodes[i]][direction]
+                if current_nodes[i][-1] == 'Z':
+                    counters[i].append(current_counters[i] + 1)
+                    current_counters[i] = 0
+                else:
+                    current_counters[i] += 1
+                if has_found_end and current_nodes[i][-1] != 'Z':
+                    has_found_end = False
+            if has_found_end:
+                break
+    for counter in counters:
+        print(counter)
+    return [counter[0] for counter in counters]
+
+
 def run():
     network, instructions = parse_data(FILENAME)
-    steps = steps_until_end(network, instructions)
+    print(steps_until_end(network, instructions))
+    periods = steps_until_end_ghost(network, instructions)
+    print(periods)
+
+    steps = periods[0]
+    keep_going = True
+    while keep_going:
+        if steps % periods[1] == 0 and steps % periods[2] == 0 and steps % periods[3] == 0 and steps % periods[4] == 0 and steps % periods[5] == 0:
+            keep_going = False
+        else:
+            steps += periods[0]
     print(steps)
 
 
