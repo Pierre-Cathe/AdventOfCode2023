a2794ca93b3ed5847f2b417cf48e3f5df5c318f0
Pierre Cathé <pierre.cathe@artelys.com>
2023-12-16 11:26:22 +0100

    Day 16 part 1

--- day16/day16.py
+++ day16/day16.py
@@ -1,7 +1,7 @@
 
 
 FILENAME = './input'
-FILENAME = './example'
+# FILENAME = './example'
 
 class Directions:
     RIGHT, LEFT, UP, DOWN = (0, 1), (0, -1), (-1, 0), (1, 0)
@@ -22,8 +22,16 @@ def simulate_beam(contraption, x, y, direction, seen_tiles_and_directions):
         return
     match contraption[x][y]:
         case '.':
-            x += direction[0]
-            y += direction[1]
+
+            tile = contraption[x][y]
+            while tile == '.':
+                seen_tiles_and_directions.add((x, y, direction))
+                x += direction[0]
+                y += direction[1]
+                if 0 <= x < len(contraption) and 0 <= y < len(contraption[0]):
+                    tile = contraption[x][y]
+                else:
+                    break
             if 0 <= x < len(contraption) and 0 <= y < len(contraption[0]):
                 simulate_beam(contraption, x, y, direction, seen_tiles_and_directions)
         case '/':
@@ -36,7 +44,50 @@ def simulate_beam(contraption, x, y, direction, seen_tiles_and_directions):
                     direction = Directions.DOWN
                 case Directions.RIGHT:
                     direction = Directions.UP
-
+            x += direction[0]
+            y += direction[1]
+            if 0 <= x < len(contraption) and 0 <= y < len(contraption[0]):
+                simulate_beam(contraption, x, y, direction, seen_tiles_and_directions)
+        case '\\':
+            match direction:
+                case Directions.UP:
+                    direction = Directions.LEFT
+                case Directions.DOWN:
+                    direction = Directions.RIGHT
+                case Directions.LEFT:
+                    direction = Directions.UP
+                case Directions.RIGHT:
+                    direction = Directions.DOWN
+            x += direction[0]
+            y += direction[1]
+            if 0 <= x < len(contraption) and 0 <= y < len(contraption[0]):
+                simulate_beam(contraption, x, y, direction, seen_tiles_and_directions)
+        case '-':
+            match direction:
+                case Directions.UP | Directions.DOWN:
+                    y1, y2 = y + 1, y - 1
+                    if 0 <= x < len(contraption) and 0 <= y1 < len(contraption[0]):
+                        simulate_beam(contraption, x, y1, Directions.RIGHT, seen_tiles_and_directions)
+                    if 0 <= x < len(contraption) and 0 <= y2 < len(contraption[0]):
+                        simulate_beam(contraption, x, y2, Directions.LEFT, seen_tiles_and_directions)
+                case Directions.RIGHT | Directions.LEFT:
+                    x += direction[0]
+                    y += direction[1]
+                    if 0 <= x < len(contraption) and 0 <= y < len(contraption[0]):
+                        simulate_beam(contraption, x, y, direction, seen_tiles_and_directions)
+        case '|':
+            match direction:
+                case Directions.RIGHT | Directions.LEFT:
+                    x1, x2 = x + 1, x - 1
+                    if 0 <= x1 < len(contraption) and 0 <= y < len(contraption[0]):
+                        simulate_beam(contraption, x1, y, Directions.DOWN, seen_tiles_and_directions)
+                    if 0 <= x2 < len(contraption) and 0 <= y < len(contraption[0]):
+                        simulate_beam(contraption, x2, y, Directions.UP, seen_tiles_and_directions)
+                case Directions.UP | Directions.DOWN:
+                    x += direction[0]
+                    y += direction[1]
+                    if 0 <= x < len(contraption) and 0 <= y < len(contraption[0]):
+                        simulate_beam(contraption, x, y, direction, seen_tiles_and_directions)
 
 
 def run():
@@ -48,6 +99,15 @@ def run():
         if (x, y) not in energised_tiles:
             energised_tiles.append((x, y))
     print(len(energised_tiles))
+    sb = []
+    for x in range(len(contraption)):
+        for y in range(len(contraption[0])):
+            if (x, y) in energised_tiles:
+                sb.append('#')
+            else:
+                sb.append('.')
+        sb.append('\n')
+    print(''.join(sb))
 
 
 if __name__ == '__main__':
